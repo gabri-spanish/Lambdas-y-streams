52805c83ede72cdebe9ba2ad6e0193415933a7c6
Gabriel Tortosa <gabrieltortosa00@gmail.com>
2026-03-27 18:37:00 +0100

    no message

--- Lambdas_y_streams/src/main/java/org/example/streams/Prueba_streams.java
+++ Lambdas_y_streams/src/main/java/org/example/streams/Prueba_streams.java
@@ -1,6 +1,5 @@
-package org.example.lambdas;
+package org.example.streams;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
--- Lambdas_y_streams/src/main/java/org/example/streams/Tarea_stream.java
+++ Lambdas_y_streams/src/main/java/org/example/streams/Tarea_stream.java
@@ -1,9 +1,8 @@
-package org.example.lambdas;
+package org.example.streams;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Tarea_stream {
 
